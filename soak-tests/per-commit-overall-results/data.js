2811475155bb93db52e627ac33368c41414e3bdf
github-action-benchmark <github@users.noreply.github.com>
2021-12-03 20:06:08 +0000

    add Soak Test Results - sample-app-springboot-auto (customSmallerIsBetter) benchmark result for e7dc6a74411f690e17abddee2fecd0a129806fc0

--- soak-tests/per-commit-overall-results/data.js
+++ soak-tests/per-commit-overall-results/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1638216667500,
+  "lastUpdate": 1638561968849,
   "repoUrl": "https://github.com/aws-observability/aws-otel-java-instrumentation",
   "entries": {
     "Soak Test Results - sample-app-spark-awssdkv1-auto": [
@@ -609,6 +609,38 @@ window.BENCHMARK_DATA = {
           {
             "name": "Soak Test Average Virtual Memory Used",
             "value": 3488.780078125,
+            "unit": "Megabytes"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Anuraag Agrawal",
+            "username": "anuraaga",
+            "email": "aanuraag@amazon.co.jp"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "e7dc6a74411f690e17abddee2fecd0a129806fc0",
+          "message": "Update to OTel 1.9 and other deps (#118)\n\n* Update to OTel 1.9 and other deps.\r\n\r\n* Update licenses\r\n\r\n* 1.9.1\r\n\r\n* Update contrib dependency and add snippet for java 17 compilation",
+          "timestamp": "2021-12-03T05:14:31Z",
+          "url": "https://github.com/aws-observability/aws-otel-java-instrumentation/commit/e7dc6a74411f690e17abddee2fecd0a129806fc0"
+        },
+        "date": 1638561968292,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Soak Test Average CPU Load",
+            "value": 27.669252873563217,
+            "unit": "Percent"
+          },
+          {
+            "name": "Soak Test Average Virtual Memory Used",
+            "value": 3471.64296875,
             "unit": "Megabytes"
           }
         ]
