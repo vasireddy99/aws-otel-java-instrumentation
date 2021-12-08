d7e7f3c8716c9e6e07be471798f44f02877e1576
github-action-benchmark <github@users.noreply.github.com>
2021-12-08 20:07:30 +0000

    add Soak Test Results - sample-app-springboot-auto (customSmallerIsBetter) benchmark result for c258373d84bc8618e3ddc7347dc7217ba534a9f8

--- soak-tests/per-commit-overall-results/data.js
+++ soak-tests/per-commit-overall-results/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1638994032391,
+  "lastUpdate": 1638994050568,
   "repoUrl": "https://github.com/aws-observability/aws-otel-java-instrumentation",
   "entries": {
     "Soak Test Results - sample-app-spark-awssdkv1-auto": [
@@ -737,6 +737,38 @@ window.BENCHMARK_DATA = {
           {
             "name": "Soak Test Average Virtual Memory Used",
             "value": 3468.82470703125,
+            "unit": "Megabytes"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "(Eliseo) Nathaniel Ruiz Nowell",
+            "username": "NathanielRN",
+            "email": "enowell@amazon.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "c258373d84bc8618e3ddc7347dc7217ba534a9f8",
+          "message": "Bump CPU threshold limit to 55 (#125)\n\n* Issue #122 showed that the CPU Usage can be as high as 55 percent at startup\r\n* Since the CPU usage is going down, we don't suspect it is a memory leak\r\n* We want to alarm on continuous HIGH CPU Usage, so we just push the limit up for now",
+          "timestamp": "2021-12-08T03:34:15Z",
+          "url": "https://github.com/aws-observability/aws-otel-java-instrumentation/commit/c258373d84bc8618e3ddc7347dc7217ba534a9f8"
+        },
+        "date": 1638994050068,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Soak Test Average CPU Load",
+            "value": 28.122261904761906,
+            "unit": "Percent"
+          },
+          {
+            "name": "Soak Test Average Virtual Memory Used",
+            "value": 3480.827182112069,
             "unit": "Megabytes"
           }
         ]
